576434c53dae50b5d45de46e78b9cc0a3cee261b
ojcarrillo <ojcarrillo@yahoo.com>
2018-02-05 13:39:39 -0500

    codigo

---
+++ src/main/java/com/example/demo/ReceiveMqApplication.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+@SpringBootApplication
+public class ReceiveMqApplication {
+	
+	private final static String QUEUE_NAME = "mq_movctas";
+
+	public static void main(String[] args) throws java.io.IOException,
+    		java.lang.InterruptedException, TimeoutException {
+		SpringApplication.run(ReceiveMqApplication.class, args);
+		
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    factory.setPort(5672);
+	    Connection connection = factory.newConnection();
+	    Channel channel = connection.createChannel();
+
+	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");	    
+	    
+	    Consumer consumer = new DefaultConsumer(channel) {
+	        @Override
+	        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+	            throws IOException {
+	          String message = new String(body, "UTF-8");
+	          System.out.println(" [x] Received '" + message + "'");
+	        }
+	      };
+	      channel.basicConsume(QUEUE_NAME, true, consumer);
+	}
+}
